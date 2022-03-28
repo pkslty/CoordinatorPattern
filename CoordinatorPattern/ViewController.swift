f34c08f8777d5e88d7ab7389353be942479f0f42
pkslty <pkslty@yandex.ru>
2022-03-28 12:16:00 +0300

    Initial commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Denis Kuzmin on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
